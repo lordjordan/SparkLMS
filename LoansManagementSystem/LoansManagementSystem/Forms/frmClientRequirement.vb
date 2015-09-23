27dc8f5234a1e2fe1d1d993d4d03d2e3fe6ff49e
Jordan Alemania <lordjordan@gmail.com>
2015-09-23 15:55:20 +0800

    9-23

---
+++ LoansManagementSystem/LoansManagementSystem/Forms/frmClientRequirement.vb
@@ -0,0 +1,11 @@
+﻿Public Class frmClientRequirement
+
+    Private Sub btnReqBack_Click(sender As Object, e As EventArgs) Handles btnReqBack.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnBrowse_Click(sender As Object, e As EventArgs)
+
+    End Sub
+
+End Class
---
+++ LoansManagementSystem/LoansManagementSystem/UserControls/Attachments.vb
@@ -0,0 +1,11 @@
+﻿Public Class Attachments
+
+    Private Sub btnClose_Click(sender As Object, e As EventArgs) Handles btnClose.Click
+        showUSC(uscMainmenu)
+    End Sub
+
+
+    Private Sub Attachments_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+End Class
