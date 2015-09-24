e98b970ae599a2142c746b5186422ab200cbbff9
eom-eom <elbertmolina_25@hotmail.com>
2015-09-24 10:32:56 +0800

    Flow updated

--- LoansManagementSystem/LoansManagementSystem/Forms/frmComaker.vb
+++ LoansManagementSystem/LoansManagementSystem/Forms/frmComaker.vb
@@ -2,9 +2,14 @@
     Dim itm As ListViewItem
     Dim db As New DBHelper("Data Source=" & My.Settings.ConString & "; Version=3;")
     Dim dr As SQLite.SQLiteDataReader
+
+    Private Sub showAddCoMaker(mode As Boolean)
+        gbxAddCoMaker.Visible = mode
+        pnlComaker.Enabled = Not mode
+    End Sub
     Private Sub btnSearchClient_Click(sender As Object, e As EventArgs) Handles btnSearchClient.Click
         'pwede imodule
-        lvCoMakerList.Items.Clear()
+        lvClientList.Items.Clear()
         Try
 
             dr = db.ExecuteReader("SELECT client_id, last_name, first_name, middle_name, branch_id, employee_no FROM tbl_clients" & _
@@ -16,7 +21,7 @@
             If dr.HasRows Then
 
                 Do While dr.Read
-                    itm = Me.lvCoMakerList.Items.Add(dr.Item("client_id").ToString)
+                    itm = Me.lvClientList.Items.Add(dr.Item("client_id").ToString)
                     itm.SubItems.Add(dr.Item("last_name").ToString & ", " & dr.Item("first_name").ToString & ", " & dr.Item("middle_name").ToString)
                     itm.SubItems.Add(dr.Item("branch_id").ToString)
                     itm.SubItems.Add(dr.Item("employee_no").ToString)
@@ -36,6 +41,23 @@
    
 
     Private Sub btnClientBack_Click(sender As Object, e As EventArgs) Handles btnClientBack.Click
-        Me.Close()
+        showAddCoMaker(False)
+    End Sub
+
+    Private Sub frmComaker_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Dim Cancel As Boolean = e.Cancel
+        Dim UnloadMode As System.Windows.Forms.CloseReason = e.CloseReason
+        If UnloadMode = CloseReason.UserClosing Then
+            Me.Hide()
+        End If
+        e.Cancel = Cancel
+    End Sub
+
+    Private Sub frmComaker_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnAddNew_Click(sender As Object, e As EventArgs) Handles btnAddNew.Click
+        showAddCoMaker(True)
     End Sub
 End Class
