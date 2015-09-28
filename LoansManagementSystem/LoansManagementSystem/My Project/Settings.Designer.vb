8b9d7d2314244d82bc5c14f213e3a07c00e45419
Jordan Alemania <lordjordan@gmail.com>
2015-09-28 10:04:20 +0800

    fixed connection string

--- LoansManagementSystem/LoansManagementSystem/My Project/Settings.Designer.vb
+++ LoansManagementSystem/LoansManagementSystem/My Project/Settings.Designer.vb
@@ -65,6 +65,18 @@ Namespace My
                 Me("ConString") = value
             End Set
         End Property
+        
+        <Global.System.Configuration.UserScopedSettingAttribute(),  _
+         Global.System.Diagnostics.DebuggerNonUserCodeAttribute(),  _
+         Global.System.Configuration.DefaultSettingValueAttribute("LMS.s3db")>  _
+        Public Property DB() As String
+            Get
+                Return CType(Me("DB"),String)
+            End Get
+            Set
+                Me("DB") = value
+            End Set
+        End Property
     End Class
 End Namespace
 
